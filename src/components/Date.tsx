8e558ebc7e2a902c3ef6dd8b1c79a3754d90180a
atkvs <vladatkishkin@vk.com>
2023-10-21 12:22:38 +0500

    diadoc-staff-timer: add react-intl

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -2,6 +2,7 @@ import { FC, useContext, useEffect, useState } from "react";
 
 import intervalToDuration from 'date-fns/intervalToDuration';
 import { DateContext } from "../App";
+import { FormattedPlural } from 'react-intl';
 
 export const DateAfterLastLeave: FC = () => {
     const [currentDate, setCurrentDate] = useState(new Date());
@@ -31,7 +32,7 @@ export const DateAfterLastLeave: FC = () => {
     
     return (
         <time>
-            <span className="date__days">{interval.days}&nbsp;дней</span> <span className="separator">/</span> <span className="date__hours">{interval.hours}&nbsp;часов</span><span className="separator">/</span> <span className="date__minutes">{interval.minutes}&nbsp;минут</span><span className="separator">/</span><span className="date__seconds">{interval.seconds}&nbsp;секунд</span>
+            <span className="date__days">{interval.days}&nbsp;<FormattedPlural value={Number(interval.days)} one="день" zero="дней" few="дня" many="дней" other="день" /></span> <span className="separator">/</span> <span className="date__hours">{interval.hours}&nbsp;<FormattedPlural value={Number(interval.hours)} one="час" zero="часов" few="часа" many="часов" other="часов" /></span><span className="separator">/</span> <span className="date__minutes">{interval.minutes}&nbsp;<FormattedPlural value={Number(interval.minutes)} one="минуту" zero="минут" few="минуты" many="минут" other="минут" /></span><span className="separator">/</span><span className="date__seconds">{interval.seconds}&nbsp;<FormattedPlural value={Number(interval.seconds)} one="секунда" zero="секунд" few="секунды" many="секунд" other="секунды" /></span>
         </time>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { THEME_2022_DARK, ThemeContext } from '@skbkontur/react-ui'
+import { IntlProvider } from 'react-intl';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeContext.Provider value={THEME_2022_DARK}>
-      <App />
-    </ThemeContext.Provider>
+    <IntlProvider locale='ru'>
+      <ThemeContext.Provider value={THEME_2022_DARK}>
+        <App />
+      </ThemeContext.Provider>
+    </IntlProvider>
   </React.StrictMode>,
 )
